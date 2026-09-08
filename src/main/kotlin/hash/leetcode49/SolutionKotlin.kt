9ae879985dd96505001134f0897c9d714ac76c20
mulehang666 <1257773943@qq.com>
2026-09-08 17:06:22 +0800

    refactor(leetcode49): 优化Kotlin实现中的哈希表初始化及返回方式

--- src/main/kotlin/hash/leetcode49/SolutionKotlin.kt
+++ src/main/kotlin/hash/leetcode49/SolutionKotlin.kt
@@ -2,13 +2,13 @@ package hash.leetcode49
 
 class SolutionKotlin {
     fun groupAnagrams(strs: Array<String>): List<List<String>> {
-        val map = HashMap<String, MutableList<String>>()
+        val map = mutableMapOf<String, MutableList<String>>()
 
         for (str in strs) {
             val sortedS = str.toCharArray()
             sortedS.sort()
             map.getOrPut(String(sortedS)) { mutableListOf() }.add(str)
         }
-        return ArrayList(map.values)
+        return map.values.toList()
     }
 }

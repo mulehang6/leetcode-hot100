e29adcfdcaef4b317b96b2dc1e4a81cff860e300
mulehang666 <1257773943@qq.com>
2026-09-08 17:25:38 +0800

    refactor(leetcode283): 优化移动零Kotlin实现中的变量命名与交换逻辑

--- src/main/kotlin/doubleptr/leetcode283/SolutionKotlin.kt
+++ src/main/kotlin/doubleptr/leetcode283/SolutionKotlin.kt
@@ -2,13 +2,14 @@ package doubleptr.leetcode283
 
 class SolutionKotlin {
     fun moveZeroes(nums: IntArray) {
-        var i0 = 0
+        var start = 0 // 0元素的下标索引
+
         for (i in nums.indices) {
             if (nums[i] != 0) {
-                val t = nums[i0]
-                nums[i0] = nums[i]
-                nums[i] = t
-                i0++
+                val t = nums[i]
+                nums[i] = nums[start]
+                nums[start] = t
+                start++
             }
         }
     }

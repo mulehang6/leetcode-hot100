8491b6b4e69158f79333699fbafbd2a61f67aca2
mulehang666 <1257773943@qq.com>
2026-09-20 17:32:55 +0800

    refactor(leetcode42): 重构接雨水Kotlin实现，拆分为前后缀分解与相向双指针解法

--- src/main/kotlin/doubleptr/leetcode42/SolutionKotlin.kt
+++ src/main/kotlin/doubleptr/leetcode42/SolutionKotlin.kt
@@ -1,16 +1,45 @@
 package doubleptr.leetcode42
 
 import kotlin.math.max
+import kotlin.math.min
 
 class SolutionKotlin {
-    fun trap(height: IntArray): Int {
-        var res = 0
-        var left = 0
-        var right = height.size - 1
-        var preMax = 0
-        var sufMax = 0
 
-        while (left < right) {
+    /**
+     * 解法一：前后缀分解
+     */
+    fun trap1(height: IntArray): Int {
+        val preMax = IntArray(height.size)
+        preMax[0] = height.first()
+        for (i in 1..height.lastIndex) {
+            preMax[i] = max(preMax[i - 1], height[i])
+        }
+
+        val sufMax = IntArray(height.size)
+        sufMax[sufMax.lastIndex] = height.last()
+        for (i in height.lastIndex - 1 downTo 0) {
+            sufMax[i] = max(sufMax[i + 1], height[i])
+        }
+
+        var res = 0
+        for (i in height.indices) {
+            res += min(preMax[i], sufMax[i]) - height[i]
+        }
+
+        return res
+    }
+
+    /**
+     * 解法2：相向双指针
+     */
+    fun trap2(height: IntArray): Int {
+        var left = 0
+        var right = height.lastIndex
+        var res = 0
+        var preMax = height.first()
+        var sufMax = height.last()
+
+        while (left <= right) {
             preMax = max(preMax, height[left])
             sufMax = max(sufMax, height[right])
 

299cf8534d055d7df15d6df3abfbe3c714bf5363
mulehang666 <1257773943@qq.com>
2026-09-20 16:29:58 +0800

    refactor(leetcode15): 优化三数之和Kotlin实现中的循环与边界条件

--- src/main/kotlin/doubleptr/leetcode15/SolutionKotlin.kt
+++ src/main/kotlin/doubleptr/leetcode15/SolutionKotlin.kt
@@ -4,32 +4,29 @@ class SolutionKotlin {
     fun threeSum(nums: IntArray): List<List<Int>> {
         nums.sort()
         val res = mutableListOf<List<Int>>()
-        for (i in 0..<nums.size - 2) {
+        for (i in 0 until nums.lastIndex - 1) {
             val x = nums[i]
             var left = i + 1
-            var right = nums.size - 1
+            var right = nums.lastIndex
 
-            // 跳过重复数字
-            if (i > 0 && nums[i - 1] == x) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue
             }
 
-            // 优化一，因为是已经排过序了
             if (x + nums[i + 1] + nums[i + 2] > 0) {
                 break
             }
 
-            // 优化二
-            if (x + nums[nums.size - 2] + nums[nums.size - 1] < 0) {
+            if (x + nums.last() + nums[nums.lastIndex - 1] < 0) {
                 continue
             }
 
             while (left < right) {
                 val sum = x + nums[left] + nums[right]
-                if (sum > 0) {
-                    right--
-                } else if (sum < 0) {
+                if (sum < 0) {
                     left++
+                } else if (sum > 0) {
+                    right--
                 } else {
                     res.add(listOf(x, nums[left], nums[right]))
                     // 两个去重
